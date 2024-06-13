6269f17aa1548db381c16ccdac00b1dd365eb43e
eduardobrl <eduardobrl10@gmail.com>
2024-06-13 20:59:20 -0300

    remove unused files and add alias

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ module "bedrock" {
   instructions     = "You are an healpful assistant that assist users of a clinic through text chat to book exams. You have access to tools tha allow consulting and scheduling exams."
   foundation_model = "amazon.titan-text-premier-v1:0"
   app_name = "maracai_assistant"
+  alias_name = "prod"
   functions = [
     {
         name = "exam_function"
