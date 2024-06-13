d890ca876ab6bb7e8f12a5ebc28b13a18b3cb98e
eduardobrl <eduardobrl10@gmail.com>
2024-06-13 19:41:58 -0300

    ajusta nomes

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ module "bedrock" {
   source = "github.com/eduardobrl/bedrock_with_function_calling_module"
   instructions     = "You are an healpful assistant that assist users of a clinic through text chat to book exams. You have access to tools tha allow consulting and scheduling exams."
   foundation_model = "amazon.titan-text-premier-v1:0"
-  app_name = "maracai"
+  app_name = "maracai_assistant"
   functions = [
     {
-        name = "test"
+        name = "exam_function"
         handler = "index.handler"
         schema = "out/exams_lambda/schema.yml"
         filename = "out/exams_lambda/code.zip"
